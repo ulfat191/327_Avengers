06355dd599e214e68277c0b7710d71c54058b0cb
Sarah-Jasim <89269184+Sarah-Jasim@users.noreply.github.com>
2022-04-16 01:46:35 +0600

    added facultySchema

---
+++ model/facultySchema.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let facultySchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String
+    },
+    university: {
+        type: String,
+        required: true
+    },
+    registrationStatus: {
+        type: Boolean,
+        required: true
+    },
+    semesterStatus: {
+        type: Boolean
+    },
+    dob: {
+        type: String
+    },
+    citizenship: {
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    address: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model('Faculty', facultySchema);
