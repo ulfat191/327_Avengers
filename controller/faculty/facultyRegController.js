d40914b8c976d7a8e4f214e4078794d1bf6c74cd
Sarah-Jasim <89269184+Sarah-Jasim@users.noreply.github.com>
2022-04-19 17:05:25 +0600

    added comments for documentation

--- controller/faculty/facultyRegController.js
+++ controller/faculty/facultyRegController.js
@@ -1,11 +1,16 @@
 // FACULTY ------------------------------------------------------------------------------------------------- (Register)
+
 const Faculty = require("../../model/facultySchema.js");
 const bcrypt = require("bcrypt");
-
+/**
+ * The function is used to register the faculty into the system
+ */
 exports.faculty_register_get = (req, res) => {
     res.render('register/faculty/faculty-register');
 }
-
+/**
+ * The function is used to register the faculty into the system, it checks whether the information given during registration is acceptable
+ */
 exports.faculty_register_post = async (req, res) => {
 
     const {email, facultyPassword, facultyPasswordConfirm} = req.body;
@@ -54,10 +59,15 @@ exports.faculty_register_post = async (req, res) => {
 }
 
 // FACULTY ------------------------------------------------------------------------------------------- (Password Reset)
+/**
+ * The function is used to enable the faculty to change their password
+ */
 exports.faculty_password_reset_get = (req, res) => {
     res.render('register/faculty/faculty-password-reset');
 }
-
+/**
+ * The faculty can use the updated password
+ */
 exports.faculty_password_reset_post = (req, res) => {
     res.redirect('faculty-password-reset');
 }
